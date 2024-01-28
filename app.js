15954a10b5571642ad0b3f738c6173f4d5b1c466
ElizabethCorrea13 <155923327+ElizabethCorrea13@users.noreply.github.com>
2024-01-28 18:04:40 -0300

    Barra de navegacion terminada

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Captura todos los enlaces del menú
-    var menuLinks = document.querySelectorAll('nav a');
+/*BARRA DE NAVEGACION*/
+function toggleMenu() {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('show');
+    
+    var menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.innerHTML = (navbar.classList.contains('show')) ? '✖' : '☰';
+}
 
-    // Añade un event listener a cada enlace
-    menuLinks.forEach(function(link) {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
+function closeMenu() {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.remove('show');
 
-            // Obtiene el objetivo del enlace (ej. #contact)
-            var targetId = this.getAttribute('href').substring(1);
+    var menuToggle = document.querySelector('.menu-toggle');
+    menuToggle.innerHTML = '☰';
+}
 
-            // Obtiene la posición y altura de la sección objetivo
-            var targetElement = document.getElementById(targetId);
-            var targetPosition = targetElement.offsetTop;
-            var targetHeight = targetElement.offsetHeight;
 
-            // Calcula la posición final y anima el desplazamiento
-            window.scrollTo({
-                top: targetPosition - targetHeight,
-                behavior: 'smooth'
-            });
-        });
-    });
-});
+/*SIGUE*/
